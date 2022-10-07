64dd6ae0bc0ebe2b23ad1a2a0578d4fde6ce5f13
SgtMarmite <d.novotny31415@gmail.com>
2022-10-07 16:44:35 +0200

    msg formating

--- src/component.py
+++ src/component.py
@@ -145,7 +145,7 @@ class Component(ComponentBase):
                     msg = json.loads(response.text)
                     logging.error(f"Reached maximum attempts when refreshing dataset: "
                                   f"error_code: {msg['error']['code']} "
-                                  f"error_message: {msg['message']['message']}")
+                                  f"error_message: {msg['error']['message']}")
                     return False
             except Exception as e:
                 logging.error(f"Dataset refresh execution failed. Exception: {e}")
